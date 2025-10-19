7e641250e2c0391530b5ccdf466e35353e157f87
Haya-biotech <hia7nahhas@gmail.com>
2025-10-20 01:38:37 +0300

    Add files via upload

---
+++ DGE.expression.R
@@ -0,0 +1,162 @@
+setwd("C:\\AI_Omics_Internship_2025")
+getwd()
+gc()
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(c("limma", "AnnotationDbi", "hgu133plus2.db"))
+
+install.packages(c("dplyr", "tibble", "ggplot2", "pheatmap"))
+
+library(AnnotationDbi)   
+library(hgu133plus2.db) 
+library(limma)           
+library(dplyr)           
+library(tibble)          
+library(ggplot2)         
+library(pheatmap) 
+installed.packages()
+setwd("C:/AI_Omics_Internship_2025/normalization.2")
+load("processed_normalized_data.RData")
+setwd("c:/AI_Omics_Internship_2025/normalization.2/raw_data_2")
+
+annotation(GSE79973_RAW)
+list.files("c:/AI_Omics_Internship_2025/normalization.2/raw_data_2")
+library(affy)
+
+cel_path <- "c:/AI_Omics_Internship_2025/normalization.2/raw_data_2/CEL_Files"
+
+raw_data <- ReadAffy(celfile.path = cel_path)
+annotation(raw_data)
+raw_data
+ls("package:hgu133plus2.db")
+columns(hgu133plus2.db)
+keytypes(hgu133plus2.db)
+probe_ids <- rownames(processed_data)
+
+gene_symbols <- mapIds(
+  hgu133plus2.db,          
+  keys = probe_ids,        
+  keytype = "PROBEID",     
+  column = "SYMBOL",      
+  multiVals = "first"      
+)
+View(gene_symbols)
+
+gene_map_df <- gene_symbols %>% as.data.frame() %>%
+  tibble::rownames_to_column("PROBEID") %>%
+  dplyr::rename(SYMBOL = 2)
+
+duplicate_summary <- gene_map_df %>%
+  group_by(SYMBOL) %>%
+  summarise(probes_per_gene = n()) %>%
+  arrange(desc(probes_per_gene))
+
+
+duplicate_genes <- duplicate_summary %>%
+  filter(probes_per_gene > 1)
+
+sum(duplicate_genes$probes_per_gene)
+
+all(gene_map_df$PROBEID == row.names(processed_data))
+
+processed_data_df <- processed_data %>%
+  as.data.frame() %>%
+  tibble::rownames_to_column("PROBEID") %>%
+  dplyr::mutate(SYMBOL = gene_symbols[PROBEID]) %>%
+  dplyr::relocate(SYMBOL, .after = PROBEID)
+processed_data_df <- processed_data_df %>%
+  dplyr::filter(!is.na(SYMBOL))
+
+expr_only <- processed_data_df %>%
+  dplyr::select(-PROBEID, -SYMBOL)
+
+averaged_data <- limma::avereps(expr_only, ID = processed_data_df$SYMBOL)
+
+x <- matrix(rnorm(8*3), 8, 3)
+colnames(x) <- c("S1", "S2", "S3")
+rownames(x) <- c("b", "a", "a", "c", "c", "b", "b", "b")
+head(x)
+avereps(x)
+dim(averaged_data)
+data <- as.data.frame(averaged_data)
+data <- data.matrix(data)
+str(data)        
+is.numeric(data) 
+
+groups <- factor(phenotype_data$source_name_ch1,
+                 levels = c("gastric mucosa", "gastric adenocarcinoma"),
+                 label = c("normal", "cancer"))
+
+class(groups)
+levels(groups)
+
+design <- model.matrix(~0 + groups)
+colnames(design) <- levels(groups)
+
+fit_1 <- lmFit(data, design)
+
+contrast_matrix <- makeContrasts(cancer_vs_normal = cancer - normal,
+                                 levels = design)
+
+fit_contrast <- contrasts.fit(fit_1, contrast_matrix)
+
+fit_2 <- eBayes(fit_contrast)
+
+deg_results <- topTable(fit_2,
+                        coef = "cancer_vs_normal",  
+                        number = Inf,               
+                        adjust.method = "BH") 
+
+deg_results$threshold <- as.factor(ifelse(
+  deg_results$adj.P.Val < 0.05 & deg_results$logFC > 1, "Upregulated",
+  ifelse(deg_results$adj.P.Val < 0.05 & deg_results$logFC < -1, "Downregulated",
+         "No")
+))
+upregulated <- subset(deg_results, threshold == "Upregulated")
+downregulated <- subset(deg_results, threshold == "Downregulated")
+
+deg_updown <- rbind(upregulated, downregulated)
+dir.create("results")
+write.csv(deg_results, file = "results/DEGs_Results.csv")
+write.csv(upregulated, file = "results/Upregulated_DEGs.csv")
+write.csv(downregulated, file = "results/Downregulated_DEGs.csv")
+write.csv(deg_updown, file = "results/Updown_DEGs.csv")
+
+png("Result_Plots/volcano_plot.png", width = 2000, height = 1500, res = 300)
+
+ggplot(deg_results, aes(x = logFC, y = -log10(adj.P.Val), color = threshold)) +
+  geom_point(alpha = 0.7, size = 2) +
+  scale_color_manual(values = c("Upregulated" = "red",
+                                "Downregulated" = "blue",
+                                "No" = "grey")) +
+  theme_minimal() +
+  labs(title = "Volcano Plot of Differentially Expressed Genes",
+       x = "log2 Fold Change",
+       y = "-log10(P-value)",
+       color = "Regulation")
+
+dev.off()
+dir.create("Result_Plots")
+
+top_genes <- head(rownames(deg_updown[order(deg_updown$adj.P.Val), ]), 25)
+heatmap_data <- data[top_genes, ]
+group_char <- as.character(groups)
+heatmap_names <- ave(group_char, group_char, FUN = function(x) paste0(x, "_", seq_along(x)))
+
+colnames(heatmap_data) <- heatmap_names
+
+png("heatmap_top50_DEGs.png", width = 2000, height = 1500, res = 300)
+pheatmap(
+  heatmap_data,
+  scale = "none", 
+  cluster_rows = FALSE,              
+  cluster_cols = TRUE,               
+  show_rownames = TRUE,              
+  show_colnames = TRUE,              
+  color = colorRampPalette(c("blue", "white", "red"))(100),
+  fontsize_row = 6,
+  fontsize_col = 8,
+  main = "Top 25 Differentially Expressed Genes"
+)
+
+dev.off()
